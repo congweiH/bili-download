c31ef4a55144c410b9bf9b26e4646c8c781146a1
congweiH <congwei_huang@163.com>
2024-08-30 00:46:05 +0800

    b站接口修改

--- src/api/BiliApi.ts
+++ src/api/BiliApi.ts
@@ -2,13 +2,13 @@
 export class BiliApi {
 
     static async webInterface(bvid: string) {
-        const res: any = await fetch(`/api/x/web-interface/view?bvid=${bvid}`);
+        const res: any = await fetch(`https://api.bilibili.com/x/web-interface/view?bvid=${bvid}`);
         const body = await res.json();
         return body.data;
     }
 
     static async player(aid: string, cid: string) {
-        const res: any = await fetch(`/api/x/player/playurl?avid=${aid}&cid=${cid}&qn=80&type=mp4&platform=html5&high_quality=1`);
+        const res: any = await fetch(`https://api.bilibili.com/x/player/playurl?avid=${aid}&cid=${cid}&qn=80&type=mp4&platform=html5&high_quality=1`);
         const body = await res.json();
         return body.data;
     }
