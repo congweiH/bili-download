627975c8e7f07026e72bc85b51f744e9b782407d
congweiH <congwei_huang@163.com>
2024-09-01 15:01:01 +0800

    增加进度

--- src/App.vue
+++ src/App.vue
@@ -8,6 +8,7 @@ import { BiliApi } from './api/BiliApi';
 
 const url = ref<string>('');
 const downloadUrl = ref<Array<any>>([]);
+const progress = ref<string>('')
 
 async function handleClick() {
 
@@ -63,22 +64,60 @@ async function getDownloadUrl() {
 
 async function download(url: string) {
   console.log(url);
-    // await fetch(url)
-  const response = await fetch(url);
-  const blob = await response.blob();
 
-  const link = document.createElement('a');
-  // 创建一个对象 URL
-  const objectUrl = URL.createObjectURL(blob);
-  link.href = objectUrl;
-  // 设置下载文件名（从响应头或者URL中获取，或者自定义）
-  link.download = 'video.mp4'; // 这里需要确保文件名和类型匹配
-  // 模拟点击链接进行下载
-  document.body.appendChild(link);
-  link.click();
-  // 清理
-  document.body.removeChild(link);
-  URL.revokeObjectURL(objectUrl);
+  // 使用函数下载视频
+  try {
+    await fetchAndDownloadVideo(url);
+  } catch (e: any) {
+    toast.error('视频下载失败！');
+    throw new Error(e);
+  }
+}
+
+async function fetchAndDownloadVideo(url: string): Promise<void> {
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    const contentLength = parseInt(response.headers.get('Content-Length') || '0', 10);
+    let downloaded = 0;
+    const reader = response.body.getReader();
+    const stream = new ReadableStream({
+      start(controller: ReadableStreamDefaultController) {
+        function push() {
+          reader.read().then(({ done, value }) => {
+            if (done) {
+              controller.close();
+              return;
+            }
+            controller.enqueue(value);
+            downloaded += value.length;
+            progress.value = (downloaded / contentLength * 100).toFixed(2) + '%';
+            push();
+          }).catch(error => {
+            console.error('Stream reading error:', error);
+            controller.error(error);
+          });
+        }
+        push();
+      }
+    });
+
+    const responseWithStream = new Response(stream, { headers: { 'Content-Type': 'video/mp4' } });
+    const blob = await responseWithStream.blob();
+
+    const objectUrl = URL.createObjectURL(blob);
+    const link = document.createElement('a');
+    link.href = objectUrl;
+    link.download = 'video.mp4'; // 这里需要确保文件名和类型匹配
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+    URL.revokeObjectURL(objectUrl);
+  } catch (error) {
+    console.error('Fetch error:', error);
+  }
 }
 
 </script>
@@ -93,6 +132,7 @@ async function download(url: string) {
       <ul v-for="url in downloadUrl">
         <span>{{ url.part }}</span>
         <Button @click="download(url.url)">下载</Button>
+        {{ progress }}
       </ul>
     </div>
   </div>
